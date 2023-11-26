3b9b0849ee80038911c970d07cdbcecfb0deb99e
charchit7 <charchitsharma11@gmail.com>
2023-11-26 19:50:41 +0530

    product arr

--- Neetcode_array_hashing/product_array.py
+++ Neetcode_array_hashing/product_array.py
@@ -1,1 +1,18 @@
 #  https://leetcode.com/problems/product-of-array-except-self/description/
+# uses the concept of prefix and postfix
+
+nums = [1,2,3,4]
+
+result = [1] * len(nums)
+
+# prefix
+for i in range(1,len(nums)):
+    result[i] = result[i-1] * nums[i-1]
+
+# postfix 
+postfix = 1
+for i in range(len(nums) -1, -1, -1):
+    result[i] = result[i] * postfix
+    postfix = postfix * nums[i]
+
+print(result)
